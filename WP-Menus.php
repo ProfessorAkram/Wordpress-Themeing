5b780f39df817e7735386e337627c58c2dc72658
Akram Taghavi-Burrsi <akram@akramsideas.com>
2013-03-05 12:54:40 -0600

    Update WP-Menus.php

--- WP-Menus.php
+++ WP-Menus.php
@@ -29,15 +29,15 @@ function to your functions.php file */
 
 /* ================================== ADDING IT TO THE THEME ====================================== */
 
-/* To place the a wp menu in your theme you will want to use one of the methods below. Again for HTML5 best practices, it is suggested that you remove the other div, with the function above and place the lines of code below inside a <nav> element within one of your theme.php pages. */
+//To place the a wp menu in your theme you will want to use one of the methods below. Again for HTML5 best practices, it is suggested that you remove the other div, with the function above and place the lines of code below inside a <nav> element within one of your theme.php pages.
 
 
-/* Placing a single menu or if more menus exsist will place the first created menu*/
+//Placing a single menu or if more menus exsist will place the first created menu
    
     <?php wp_nav_menu(); ?>
 
 
-/* To place a sepcific menu you will need to use the code below, where 'Menu Name' is the name you set for the menu */
+//To place a sepcific menu you will need to use the code below, where 'Menu Name' is the name you set for the menu
     
     <?php wp_nav_menu( array('menu' => 'Menu Name ' )); ?>
 
